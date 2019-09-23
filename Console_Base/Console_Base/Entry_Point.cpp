612f49e9896cb4f96c0b6cf2ac1a54ed6e81f7a9
Uniquekittchen <55697839+Uniquekittchen@users.noreply.github.com>
2019-09-23 15:36:02 +0300

    Update Entry_Point.cpp
    fixed code issue

--- Console_Base/Console_Base/Entry_Point.cpp
+++ Console_Base/Console_Base/Entry_Point.cpp
@@ -5,7 +5,7 @@ BOOL WINAPI Initialize(HINSTANCE h_Module)
 	Console->Start_Attaching();
 	Console->Log_Color("Example", e_Colors::Red);
 	Console->Log_Color("Example Non-Color", e_Colors::White);
-	Console->Start_Attaching();
+	Console->Stop_Attaching();
 
 	FreeLibraryAndExitThread(h_Module, EXIT_SUCCESS);
 }
